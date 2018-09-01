aa418e855181663058eb1a84ad56d074872f90a9
GcsSloop <GcsSloop@gmail.com>
2018-09-01 22:51:28 +0800

    1. 添加空项目

---
+++ app/src/main/java/com/gcssloop/arcseekbar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gcssloop.arcseekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
